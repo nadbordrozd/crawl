a93396cab219827cee9120c5e3d73c2a319e07e1
Nadbor Drozd <nadbordrozd@gmail.com>
2025-06-14 14:58:08 +0100

    defined more sprites

--- scripts/sprites.js
+++ scripts/sprites.js
@@ -19,7 +19,24 @@ const SPRITES = {
     assassin: [112, 528],
     bomb: [1184, 544],
     audrey_2: [96, 480],
-    gate: [80, 336]
+    gate: [80, 336],
+    NS_wall: [64, 304],
+    NW_corner: [80, 304],
+    WE_wall: [96, 304],
+    NE_corner: [112, 304],
+    t_wall: [128, 304],
+    reverse_t_wall: [128, 320],
+    grate_wall: [64, 320],
+    SW_corner: [80, 320],
+    torch_wall: [96, 320],
+    SE_corner: [112, 320],
+    tower: [96, 288],
+    diamond: [80, 592]
+
+
+
+
+
 };
 
 // For CommonJS/Node.js style
